5e2e13c315ebe74c510828de454671bfec9c0b74
Reece Russell <reece@prominentmedia.com>
2020-05-18 19:03:48 +0100

    ironed out bugs

--- Core/Core/Abstractions/SecretProvider.cs
+++ Core/Core/Abstractions/SecretProvider.cs
@@ -6,8 +6,11 @@ using Org.BouncyCastle.OpenSsl;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Net.Mime;
 using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
+using Org.BouncyCastle.Crypto;
 
 namespace Core.Abstractions
 {
@@ -15,13 +18,16 @@ namespace Core.Abstractions
     {
         private readonly IAmazonSecretsManager _client;
 
-        public SecretProvider()
+        public SecretProvider(IAmazonSecretsManager client)
         {
-            _client = new AmazonSecretsManagerClient();
+            _client = client;
         }
 
         public async Task<RSA> GetRsa(string name, string key)
         {
+            if (string.IsNullOrEmpty(name)) throw new ArgumentNullException(nameof(name));
+            if (string.IsNullOrEmpty(key)) throw new ArgumentNullException(nameof(key));
+
             string secret;
             var response = await _client.GetSecretValueAsync(new GetSecretValueRequest {SecretId = name});
             if (response.SecretString != null)
@@ -32,18 +38,40 @@ namespace Core.Abstractions
             {
                 await using var memoryStream = response.SecretBinary;
                 using var reader = new StreamReader(memoryStream);
-                secret = System.Text.Encoding.UTF8.GetString(Convert.FromBase64String(reader.ReadToEnd()));
+                secret = Encoding.UTF8.GetString(Convert.FromBase64String(reader.ReadToEnd()));
             }
 
             var content = JsonConvert.DeserializeObject<IDictionary<string, string>>(secret);
-            using var keyReader = new StringReader(content[key]);
-            var publicKeyParam = (RsaKeyParameters)new PemReader(keyReader).ReadObject();
+            using var keyReader = new StringReader(FormatRsaData(content[key]));
+            var asymmetricKeyPair = (AsymmetricCipherKeyPair)new PemReader(keyReader).ReadObject();
+            var privateKeyParams = (RsaPrivateCrtKeyParameters) asymmetricKeyPair.Private;
 
             return RSA.Create(new RSAParameters
             {
-                Modulus = publicKeyParam.Modulus.ToByteArrayUnsigned(),
-                Exponent = publicKeyParam.Exponent.ToByteArrayUnsigned()
+                Modulus = privateKeyParams.Modulus.ToByteArrayUnsigned(),
+                P = privateKeyParams.P.ToByteArrayUnsigned(),
+                Q = privateKeyParams.Q.ToByteArrayUnsigned(),
+                DP = privateKeyParams.DP.ToByteArrayUnsigned(),
+                DQ = privateKeyParams.DQ.ToByteArrayUnsigned(),
+                InverseQ = privateKeyParams.QInv.ToByteArrayUnsigned(),
+                D = privateKeyParams.Exponent.ToByteArrayUnsigned(),
+                Exponent = privateKeyParams.PublicExponent.ToByteArrayUnsigned()
             });
+        }
+
+        private static string FormatRsaData(string content)
+        {
+            var parts = content.Split(" ");
+            var partCount = parts.Length;
+
+            var data = string.Join(" ", parts[Range.EndAt(4)]);
+
+            for (var i = 4; i < partCount - 4; i++)
+            {
+                data += "\n" + parts[i];
+            }
+
+            return data + "\n" + string.Join(" ", parts[Range.StartAt(partCount - 4)]);
         }
 
         public async Task<string> GetMySqlConnectionStringAsync(string name)
@@ -58,7 +86,7 @@ namespace Core.Abstractions
             {
                 await using var memoryStream = response.SecretBinary;
                 using var reader = new StreamReader(memoryStream);
-                secret = System.Text.Encoding.UTF8.GetString(Convert.FromBase64String(reader.ReadToEnd()));
+                secret = Encoding.UTF8.GetString(Convert.FromBase64String(reader.ReadToEnd()));
             }
 
             var content = JsonConvert.DeserializeObject<IDictionary<string, string>>(secret);
