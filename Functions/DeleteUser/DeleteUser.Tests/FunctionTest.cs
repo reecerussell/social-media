dd8ffc4beca27ab0afbef4b2fd7dd495577c5397
Reece Russell <reece@prominentmedia.com>
2020-05-19 11:48:32 +0100

    added delete user function

---
+++ Functions/DeleteUser/DeleteUser.Tests/FunctionTest.cs
@@ -0,0 +1,68 @@
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.TestUtilities;
+using Core.Dtos;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace DeleteUser.Tests
+{
+    public class FunctionTest
+    {
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public FunctionTest(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+
+            Environment.SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", "test", EnvironmentVariableTarget.Process);
+            Environment.SetEnvironmentVariable("EXCEPTION_MODE", "detailed", EnvironmentVariableTarget.Process);
+            Environment.SetEnvironmentVariable("AWS_REGION", "eu-west-2", EnvironmentVariableTarget.Process);
+        }
+
+        /// <summary>
+        /// This is a specific text for a pre-existing user.
+        /// </summary>
+        [Theory]
+        [InlineData("7f83a0af-e7f4-4679-b600-ec23ce861a9c")]
+        public async Task TestChangeUsername(string userId)
+        {
+            var context = new TestLambdaContext();
+            var request = new APIGatewayProxyRequest { Headers = MockHeaders(userId) };
+
+            var data = new UpdateUsernameDto
+            {
+                Username = "MyNewUsername",
+            };
+            request.Body = JsonConvert.SerializeObject(data);
+
+            var functions = new Functions();
+            var response = await functions.DeleteUser(request, context);
+
+            _testOutputHelper.WriteLine("Body: " + response.Body);
+
+            Assert.Equal(200, response.StatusCode);
+            Assert.Null(response.Body);
+        }
+
+        private static IDictionary<string, string> MockHeaders(string userId)
+        {
+            var payload = new Dictionary<string, string>
+            {
+                {"user_id", userId}
+            };
+
+            var payloadJson = JsonConvert.SerializeObject(payload);
+            var payloadData = Convert.ToBase64String(Encoding.UTF8.GetBytes(payloadJson));
+
+            return new Dictionary<string, string>
+            {
+                {"Authorization", $"buffer.{payloadData}.buffer"}
+            };
+        }
+    }
+}
---
+++ Functions/DeleteUser/DeleteUser/Function.cs
@@ -0,0 +1,48 @@
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+using Core;
+using CSharpFunctionalExtensions;
+using System;
+using System.Threading.Tasks;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace DeleteUser
+{
+    public class Functions
+    {
+        private readonly IUserManager _userManager;
+
+        public Functions()
+        {
+            var services = Container.Build();
+            _userManager = (IUserManager)services.GetService(typeof(IUserManager));
+        }
+
+        public async Task<APIGatewayProxyResponse> DeleteUser(APIGatewayProxyRequest request, ILambdaContext lambdaContext)
+        {
+            using var context = new Context(request, true);
+
+            try
+            {
+                var (success, _, error) = await _userManager.DeleteAsync(context.GetUserId());
+                if (!success)
+                {
+                    if (CommonErrors.IsNotFound(error))
+                    {
+                        return request.NotFound(context, error);
+                    }
+
+                    return request.BadRequest(context, error);
+                }
+
+                return request.Ok(context, null);
+            }
+            catch (Exception e)
+            {
+                return request.Error(context, e);
+            }
+        }
+    }
+}
